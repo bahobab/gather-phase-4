7b8eb3bc8b49a9d4e464d887fbd0c8c11e0f7333
mono motapa <bahobab@hotmail.com>
2018-01-09 14:59:47 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const Item = require('../models/item');
+
+router.get('/', async (req, res, next) => {
+  const items = await Item.find({});
+  res.render('index', {items});
+});
+
+router.get('/items/create', async (req, res, next) => {
+  res.render('create');
+});
+
+router.post('/items/create', async (req, res, next) => {
+  const {title, description, imageUrl} = req.body;
+  const newItem = new Item({title, description, imageUrl});
+  newItem.validateSync();
+  if (newItem.errors) {
+    res.status(400).render('create', {newItem: newItem});
+  } else {
+    await newItem.save();
+    res.redirect('/');
+  }
+});
+
+router.get('/items/:itemid', async (req, res, next) => {
+	const itemId = req.params.itemid;
+	const item = await Item.findById(itemId);
+//	console.log('<<<<<>>>', item.description);
+	const html = '<div id="item-title" class="item-card"><a href="#">' + item.title + '</a></div><div id="item-description">' + item.description + '</div>'
+	res.send(html);
+//	res.render('index', {
+//		title: item.title,
+//		description: item.title,
+//		imageUrl: item.imageUrl
+//	});
+});
+
+module.exports = router;
+ 
---
+++ test/routes/single-item-test.js
@@ -0,0 +1,27 @@
+const {assert} = require('chai');
+const request = require('supertest');
+
+const app = require('../../app');
+
+const {parseTextFromHTML, seedItemToDatabase} = require('../test-utils');
+const {connectDatabaseAndDropData, diconnectDatabase} = require('../setup-teardown-utils');
+
+describe('Server path: /items/:id', () => {
+  beforeEach(connectDatabaseAndDropData);
+
+  afterEach(diconnectDatabase);
+
+	// Write your test blocks below:
+	describe('Item details', () => {
+		
+		it('renders a selected item', async () => {
+			
+			const item = await seedItemToDatabase();
+			const response = await request(app)
+															.get(`/items/${item._id}`);
+
+			assert.include(item.title, parseTextFromHTML(response.text, '#item-title'));
+			assert.include(item.description, parseTextFromHTML(response.text, '#item-description'));
+		});
+	});
+});
